de70e2ad092fde14df44ec92a24944c32b5e121f
Robin Schmidt <m20210602@novaims.unl.pt>
2022-05-29 23:38:17 +0100

    gauge plot code added

--- BC5/BC5_EA.py
+++ BC5/BC5_EA.py
@@ -456,6 +456,62 @@ def get_predictions(coin = 'BTC-USD'):
     pred_tomorrow2 = "The prediction for day after tomorrow is that " + coin + "'s price is: " + price_tmr2
     return pred_tomorrow, pred_tomorrow2
 
+def gauge_plot(predicted_value, current_value, interval=0.05):
+    plot_bgcolor = "#def"
+    quadrant_colors = [plot_bgcolor, "#f25829", "#eff229", "#2bad4e"]
+    quadrant_text = ["", "<b>Sell</b>", "<b>Hold</b>", "<b>Buy</b>"]
+    n_quadrants = len(quadrant_colors) - 1
+
+    min_value = predicted_value * (1 - interval)
+    max_value = predicted_value * (1 + interval)
+    hand_length = np.sqrt(2) / 4
+    hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_value) / (max_value - min_value))
+
+    fig = go.Figure(
+        data=[
+            go.Pie(
+                values=[0.5] + (np.ones(n_quadrants) / 2 / n_quadrants).tolist(),
+                rotation=90,
+                hole=0.5,
+                marker_colors=quadrant_colors,
+                text=quadrant_text,
+                textinfo="text",
+                hoverinfo="skip",
+            ),
+        ],
+        layout=go.Layout(
+            showlegend=False,
+            margin=dict(b=0, t=10, l=10, r=10),
+            width=450,
+            height=450,
+            paper_bgcolor=plot_bgcolor,
+            annotations=[
+                go.layout.Annotation(
+                    text=f"<b>CoinName</b>",
+                    x=0.5, xanchor="center", xref="paper",
+                    y=0.25, yanchor="bottom", yref="paper",
+                    showarrow=False,
+                )
+            ],
+            shapes=[
+                go.layout.Shape(
+                    type="circle",
+                    x0=0.48, x1=0.52,
+                    y0=0.48, y1=0.52,
+                    fillcolor="#333",
+                    line_color="#333",
+                ),
+                go.layout.Shape(
+                    type="line",
+                    x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
+                    y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
+                    line=dict(color="#333", width=4)
+                )
+            ]
+        )
+    )
+    return fig
+
 # ----------------------------------------------------------------------------------------------------------------------
 # App Layout
 
